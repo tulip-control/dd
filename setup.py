16ce97cdd0c9bbfabaa0b3fb3a7e7d3a723ca2f1
Ioannis Filippidis <jfilippidis@gmail.com>
2015-01-26 11:28:53 -0800

    improve setup.py
    - load readme from markdown only (conversion to rst not worth the effort: read it on github)
    - use numpy version practices, writing a version file
    - place pydot and tulip in extras

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
-import warnings
 
 
+description = 'Library of decision diagrams and algorithms on them.'
 README = 'README.md'
-try:
-    long_description = open(README).read()
-except:
-    warnings.warn('Could not find {readme}'.format(readme=README))
+VERSION_FILE = 'dd/version.py'
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+version = '{major}.{minor}.{micro}'.format(
+    major=MAJOR, minor=MINOR, micro=MICRO)
+s = (
+    '# This file was generated from setup.py\n'
+    "version = '{version}'\n").format(version=version)
 
 
-setup(
-    name='dd',
-    version='0.0.2',
-    license='BSD',
-    description='Library of decision diagrams and algorithms on them.',
-    long_description=long_description,
-    author='Ioannis Filippidis',
-    author_email='jfilippidis@gmail.com',
-    url='https://github.com/johnyf/dd',
-    install_requires=['networkx'],
-    keywords=['bdd', 'decision diagram', 'networkx', 'boolean'])
+if __name__ == '__main__':
+    with open(VERSION_FILE, 'w') as f:
+        f.write(s)
+    setup(
+        name='dd',
+        version=version,
+        description=description,
+        long_description=open(README).read(),
+        author='Ioannis Filippidis',
+        author_email='jfilippidis@gmail.com',
+        url='https://github.com/johnyf/dd',
+        license='BSD',
+        install_requires=['networkx >= 1.9.1', 'ply == 3.4'],
+        extras_require={'dot': 'pydot', 'expr': 'tulip >= 1.2.dev'},
+        tests_require=['nose'],
+        packages=['dd'],
+        package_dir={'dd': 'dd'},
+        keywords=['bdd', 'binary decision diagram',
+                  'decision diagram', 'networkx', 'boolean'])

fc17afa7ceb8bdc1fb473b76081497c1a5ef2c1a
Ioannis Filippidis <jfilippidis@gmail.com>
2015-03-28 03:16:30 -0700

    refactor setup.py
    - use `ply_required` to specify the PLY version at a single place.
    - make `setup.py` easily reusable

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import pip
 from setuptools import setup
 
 
+name = 'dd'
 description = 'Library of decision diagrams and algorithms on them.'
+url = 'https://github.com/johnyf/{name}'.format(name=name)
 README = 'README.md'
-VERSION_FILE = 'dd/version.py'
+VERSION_FILE = '{name}/version.py'.format(name=name)
 MAJOR = 0
 MINOR = 0
 MICRO = 3
@@ -13,33 +15,37 @@ version = '{major}.{minor}.{micro}'.format(
 s = (
     '# This file was generated from setup.py\n'
     "version = '{version}'\n").format(version=version)
+ply_required = 'ply >= 3.4'
+install_requires = [ply_required]
+extras_require = {
+    'dot': 'pydot >= 1.0.28',
+    'nx': 'networkx >= 1.9.1'}
+tests_require = [
+    'nose >= 1.3.4',
+    'networkx >= 1.9.1',
+    'pydot >= 1.0.28']
 
 
 if __name__ == '__main__':
-    pip.main(['install', 'ply == 3.4'])
     with open(VERSION_FILE, 'w') as f:
         f.write(s)
+    pip.main(['install', ply_required])
     from dd import dddmp
-    dddmp._rewrite_tables('dd/')
+    dddmp._rewrite_tables(outputdir=name)
     setup(
-        name='dd',
+        name=name,
         version=version,
         description=description,
         long_description=open(README).read(),
         author='Ioannis Filippidis',
         author_email='jfilippidis@gmail.com',
-        url='https://github.com/johnyf/dd',
+        url=url,
         license='BSD',
-        install_requires=['ply == 3.4'],
-        extras_require={
-            'dot': 'pydot >= 1.0.28',
-            'nx': 'networkx >= 1.9.1',
-        tests_require=[
-            'nose >= 1.3.4',
-            'networkx >= 1.9.1',
-            'pydot >= 1.0.28',
-        packages=['dd'],
-        package_dir={'dd': 'dd'},
+        install_requires=install_requires,
+        extras_require=extras_require,
+        tests_require=tests_require,
+        packages=[name],
+        package_dir={name: name},
         keywords=[
             'bdd', 'binary decision diagram', 'decision diagram',
             'boolean', 'networkx', 'dot'])

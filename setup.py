5a6097e892a3a69c6e50ab904d173d9326bc7288
Ioannis Filippidis <jfilippidis@gmail.com>
2017-02-11 17:02:07 -0800

    REL: bump version to 0.4.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ README = 'README.md'
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 4
-MICRO = 2
+MICRO = 3
 VERSION = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 VERSION_TEXT = (

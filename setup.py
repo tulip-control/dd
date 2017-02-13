137b23b428f5ad834830c3167c6869f89e45bb70
Ioannis Filippidis <jfilippidis@gmail.com>
2017-02-13 10:57:49 -0800

    REL: bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ url = 'https://github.com/johnyf/{name}'.format(name=name)
 README = 'README.md'
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
-MINOR = 4
-MICRO = 3
+MINOR = 5
+MICRO = 0
 VERSION = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 VERSION_TEXT = (

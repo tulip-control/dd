tulip-control/dd

commit 984e32e09825fca66a52813b20f3871beb123ae6

ENH: add method `BDD.succ` for getting `(level, low, high)`
as a unified interface among `cudd`, `autoref`, and `bdd`.

dd/autoref.py
@@ -53,6 +53,12 @@ class BDD(object):
         assert u in self._bdd
         return Function(u, self._bdd)
 
+    def succ(self, u):
+        i, v, w = self._bdd.succ(u)
+        v = self._wrap(v)
+        w = self._wrap(w)
+        return i, v, w
+
     def add_var(self, var, level=None):
         return self._bdd.add_var(var, level=level)
 

dd/bdd.py
@@ -130,6 +130,10 @@ class BDD(object):
         self._succ[1] = (i, None, None)
         self._ref.setdefault(1, 0)
 
+    def succ(self, u):
+        """Return `(level, low, high)` for `u`."""
+        return self._succ[abs(u)]
+
     def incref(self, u):
         """Increment reference count of node `u`."""
         self._ref[abs(u)] += 1

dd/cudd.pyx
@@ -486,6 +486,13 @@ cdef class BDD(object):
                     'Unknown parameter "{k}"'.format(k=k))
         return d
 
+    cpdef succ(self, Function u):
+        """Return `(level, low, high)` for `u`."""
+        i = u.level
+        v = u.low
+        w = u.high
+        return i, v, w
+
     cdef incref(self, DdNode *u):
         Cudd_Ref(u)
 

commit 5175d9878f4c68abf5e2004a38f56e8ebc0be1c0

BUG: pass `u.node` to method `bdd.BDD.succ` from `autoref.BDD.succ`
introduced in 984e32e09825fca66a52813b20f3871beb123ae6.

dd/autoref.py
@@ -60,7 +60,7 @@ class BDD(object):
         return self._bdd.configure(**kw)
 
     def succ(self, u):
-        i, v, w = self._bdd.succ(u)
+        i, v, w = self._bdd.succ(u.node)
         v = self._wrap(v)
         w = self._wrap(w)
         return i, v, w
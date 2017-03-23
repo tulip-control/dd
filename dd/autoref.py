tulip-control/dd

commit d63159cd4b92e96bc142b577cece44bc59d254d5

ENH: add method `BDD.copy`
because it allows the same code to run on a `BDD` without caring whether it is from `autoref`, `bdd`, or `cudd`.

dd/autoref.py
@@ -66,6 +66,10 @@ class BDD(object):
     def level_of_var(self, var):
         return self._bdd.level_of_var(var)
 
+    def copy(self, u, other):
+        r = self._bdd.copy(u.node, other)
+        return self._wrap(r)
+
     def evaluate(self, u, values):
         assert u in self, u
         return self._bdd.evaluate(u.node, values)

dd/bdd.py
@@ -222,6 +222,14 @@ class BDD(object):
     def _top_var(self, *nodes):
         return min(map(lambda x: self._succ[abs(x)][0], nodes))
 
+    def copy(self, u, other):
+        """Transfer BDD with root `u` to `other`.
+
+        @type other: `BDD`
+        @rtype: node
+        """
+        return copy_bdd(u, self, other)
+
     def descendants(self, u):
         """Return BDD nodes reachable from node `u`.
 

dd/cudd.pyx
@@ -580,6 +580,14 @@ cdef class BDD(object):
         assert set(_compat.values(supp)) == {True}, supp
         return set(supp)
 
+    def copy(self, u, other):
+        """Transfer BDD with root `u` to `other`.
+
+        @type other: `BDD`
+        @rtype: node
+        """
+        return copy_bdd(u, self, other)
+
     cpdef Function cofactor(self, Function f, values):
         """Return the cofactor f|_g."""
         assert self.manager == f.manager

commit c5beb7faea50a9773e5490195c37d0fff54fb9e4

BUG: pass `other.bdd` in `autoref.BDD.copy`
introduced in d63159cd4b92e96bc142b577cece44bc59d254d5.

dd/autoref.py
@@ -79,7 +79,7 @@ class BDD(object):
         return self._bdd.level_of_var(var)
 
     def copy(self, u, other):
-        r = self._bdd.copy(u.node, other)
+        r = self._bdd.copy(u.node, other._bdd)
         return self._wrap(r)
 
     def support(self, u, as_levels=False):
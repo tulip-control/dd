a6e1702e5d673d6c080222d0411e3e277565026f
CUDD <>
2018-01-17 23:49:16 -0800

    ENH: copy excerpt from `cudd/cuddCompose.c` to `dd/_cudd_addendum.c`
    The file `cuddCompose.c` is part of CUDD version 3.0.0 [1].
    License information has been included at the top of `_cudd_addendum.c`.
    
    [1] ftp://vlsi.colorado.edu/pub/cudd-3.0.0.tar.gz

--- dd/_cudd_addendum.c
+++ dd/_cudd_addendum.c
@@ -3,9 +3,9 @@
 
   @ingroup cudd
 
-  @brief Excerpt from `cudd/cuddBridge.c`.
+  @brief Excerpts from `cudd/cuddBridge.c` and `cudd/cuddCompose.c`.
 
-  @author Fabio Somenzi
+  @author Fabio Somenzi and Kavita Ravi
 
   @copyright@parblock
   Copyright (c) 1995-2015, Regents of the University of Colorado
@@ -220,3 +220,138 @@ cuddBddTransferRecur(
     return(Cudd_NotCond(res,comple));
 
 } /* end of cuddBddTransferRecur */
+
+
+
+/**
+  @brief Permutes the variables of a %BDD.
+
+  @details Given a permutation in array permut, creates a new %BDD
+  with permuted variables. There should be an entry in array permut
+  for each variable in the manager. The i-th entry of permut holds the
+  index of the variable that is to substitute the i-th variable.
+
+  @return a pointer to the resulting %BDD if successful; NULL
+  otherwise.
+
+  @sideeffect None
+
+  @see Cudd_addPermute Cudd_bddSwapVariables
+
+*/
+DdNode *
+Cudd_bddPermute(
+  DdManager * manager,
+  DdNode * node,
+  int * permut)
+{
+    DdHashTable		*table;
+    DdNode		*res;
+
+    do {
+	manager->reordered = 0;
+	table = cuddHashTableInit(manager,1,2);
+	if (table == NULL) return(NULL);
+	res = cuddBddPermuteRecur(manager,table,node,permut);
+	if (res != NULL) cuddRef(res);
+	/* Dispose of local cache. */
+	cuddHashTableQuit(table);
+
+    } while (manager->reordered == 1);
+
+    if (res != NULL) cuddDeref(res);
+    if (manager->errorCode == CUDD_TIMEOUT_EXPIRED && manager->timeoutHandler) {
+        manager->timeoutHandler(manager, manager->tohArg);
+    }
+    return(res);
+
+} /* end of Cudd_bddPermute */
+
+
+/**
+  @brief Implements the recursive step of Cudd_bddPermute.
+
+  @details Recursively puts the %BDD in the order given in the array permut.
+  Checks for trivial cases to terminate recursion, then splits on the
+  children of this node.  Once the solutions for the children are
+  obtained, it puts into the current position the node from the rest of
+  the %BDD that should be here. Then returns this %BDD.
+  The key here is that the node being visited is NOT put in its proper
+  place by this instance, but rather is switched when its proper position
+  is reached in the recursion tree.<p>
+  The DdNode * that is returned is the same %BDD as passed in as node,
+  but in the new order.
+
+  @sideeffect None
+
+  @see Cudd_bddPermute cuddAddPermuteRecur
+
+*/
+static DdNode *
+cuddBddPermuteRecur(
+  DdManager * manager /**< %DD manager */,
+  DdHashTable * table /**< computed table */,
+  DdNode * node /**< %BDD to be reordered */,
+  int * permut /**< permutation array */)
+{
+    DdNode	*N,*T,*E;
+    DdNode	*res;
+    int		index;
+
+    statLine(manager);
+    N = Cudd_Regular(node);
+
+    /* Check for terminal case of constant node. */
+    if (cuddIsConstant(N)) {
+	return(node);
+    }
+
+    /* If problem already solved, look up answer and return. */
+    if (N->ref != 1 && (res = cuddHashTableLookup1(table,N)) != NULL) {
+#ifdef DD_DEBUG
+	manager->bddPermuteRecurHits++;
+#endif
+	return(Cudd_NotCond(res,N != node));
+    }
+
+    /* Split and recur on children of this node. */
+    T = cuddBddPermuteRecur(manager,table,cuddT(N),permut);
+    if (T == NULL) return(NULL);
+    cuddRef(T);
+    E = cuddBddPermuteRecur(manager,table,cuddE(N),permut);
+    if (E == NULL) {
+	Cudd_IterDerefBdd(manager, T);
+	return(NULL);
+    }
+    cuddRef(E);
+
+    /* Move variable that should be in this position to this position
+    ** by retrieving the single var BDD for that variable, and calling
+    ** cuddBddIteRecur with the T and E we just created.
+    */
+    index = permut[N->index];
+    res = cuddBddIteRecur(manager,manager->vars[index],T,E);
+    if (res == NULL) {
+	Cudd_IterDerefBdd(manager, T);
+	Cudd_IterDerefBdd(manager, E);
+	return(NULL);
+    }
+    cuddRef(res);
+    Cudd_IterDerefBdd(manager, T);
+    Cudd_IterDerefBdd(manager, E);
+
+    /* Do not keep the result if the reference count is only 1, since
+    ** it will not be visited again.
+    */
+    if (N->ref != 1) {
+	ptrint fanout = (ptrint) N->ref;
+	cuddSatDec(fanout);
+	if (!cuddHashTableInsert1(table,N,res,fanout)) {
+	    Cudd_IterDerefBdd(manager, res);
+	    return(NULL);
+	}
+    }
+    cuddDeref(res);
+    return(Cudd_NotCond(res,N != node));
+
+} /* end of cuddBddPermuteRecur */

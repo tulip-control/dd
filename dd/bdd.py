f5774176d2c4c4226134373d448a80c3088c8f43
Ioannis Filippidis <jfilippidis@gmail.com>
2018-01-22 04:52:22 -0800

    API: rm overconstraining assertions from `copy_bdd`
    The function `copy_bdd` works also for managers
    with variables whose levels form a set of non-contiguous
    integers.

--- dd/bdd.py
+++ dd/bdd.py
@@ -1741,8 +1741,6 @@ def copy_bdd(u, from_bdd, to_bdd):
     if from_bdd is to_bdd:
         logger.warning('copying node to same manager')
         return u
-    _assert_valid_ordering(from_bdd.vars)
-    _assert_valid_ordering(to_bdd.vars)
     support = from_bdd.support(u)
     level_map = _level_map(from_bdd, to_bdd, support)
     cache = dict()

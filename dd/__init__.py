682f66d298cd0ddf1da4f35834c7150330c2e91b
Ioannis Filippidis <jfilippidis@gmail.com>
2018-03-04 13:51:56 +0200

    API: use `dd.cudd.BDD` as `dd.BDD` when possible
    and `dd.autoref.BDD` otherwise. This approach makes
    available the high-level interface to the user, and
    seamlessly switches to a more efficient implementation,
    when one is available, similarly to how `pickle` works.

--- dd/__init__.py
+++ dd/__init__.py
@@ -1,6 +1,10 @@
 """Package of algorithms based on decision diagrams."""
-from dd.bdd import BDD
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = None
+try:
+    from dd import cudd as _bdd
+except ImportError:
+    from dd import autoref as _bdd
+BDD = _bdd.BDD

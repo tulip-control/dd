0f6d16483cc13078edebac9e89d1d4b99d22991e
Scott C. Livingston <slivingston@cds.caltech.edu>
2019-04-22 09:40:43 -0700

    TEST: regression test for #40

---
+++ tests/regressions_test.py
@@ -0,0 +1,22 @@
+from dd import cudd
+
+
+def test_reordering_setting_restore():
+    # Original report at https://github.com/tulip-control/dd/issues/40
+    b = cudd.BDD()
+    b.configure(reordering=False)
+    b.add_var('x')
+    b.add_var('y')
+    # x /\ y
+    s = '~ x /\ y'
+    u = b.add_expr(s)
+    assert not b.configure()['reordering']
+    g = b.pick_iter(u)
+    m = list(g)
+    m_ = [dict(x=False, y=True)]
+    assert m == m_, (m, m_)
+    assert not b.configure()['reordering']
+
+
+if __name__ == '__main__':
+    test_reordering_setting_restore()

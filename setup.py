140329ef5ad8a56883ed7d1d7a7cffcfcd4b7fdc
Ioannis Filippidis <jfilippidis@gmail.com>
2020-02-21 01:38:31 +0200

    REL: bump version to 0.5.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ url = 'https://github.com/tulip-control/{name}'.format(name=name)
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 5
-MICRO = 5
+MICRO = 6
 VERSION = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 VERSION_TEXT = (

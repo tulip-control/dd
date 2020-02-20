tulip-control/dd

commit 4ea0e9ae4373ee599163772dec0784c3497a50b3

API: allow method `cudd.BDD.rename` to swap variables
because `Cudd_bddSwapVariables` supports swapping.

dd/cudd.pyx
@@ -721,10 +721,6 @@ cdef class BDD(object):
         target_vars = dvars.values()
         common = set(dvars).intersection(target_vars)
         assert not common, common
-        # assert new vars not in support
-        support = self.support(u)
-        common = support.intersection(target_vars)
-        assert not common, common
         # rename
         n = len(dvars)
         cdef DdNode **x = <DdNode **> PyMem_Malloc(n * sizeof(DdNode *))

commit e2dcfe795574fbd4bcd4510386cdb23fd23b98a2

BUG: correctly rename variables in `cudd.BDD._rename`
For the call `_rename(u, d)`, if `d['x'] = 'y'` and the BDD `u`
depends on the variable `'y'`, then calling `Cudd_bddSwapVariables`:

1. replaces `'x'` with `'y'` in `u` (as intended)
2. replaces `'y'` with `'x'` in `u` (unintended)

Instead, use composition.

Move the previous implementation of the method `cudd.BDD._rename` to
a new method `cudd.BDD._swap`. The new method asserts that each
variable occurs in at most one key-value pair of the dictionary
`dvars` given as argument.

The implementation of the method `BDD._rename` calls `BDD.var` for
each value of the dictionary `dvars`. The previous implementation
of `BDD._rename` (now the implementation of `BDD._swap`) calls
`BDD.var` for each key and value of `dvars`.

Update the tests of `BDD._rename` to catch this bug.
Test the new method `BDD._swap`.

Comparison to previous changes
==============================

This change partially reverts b3ea0ec6fe27d8900327cd01b128e9e21aee1064,
which removed the assertion that keys and values of `dvars` are
disjoint sets.

The assertion from b3ea0ec6fe27d8900327cd01b128e9e21aee1064 is indeed
restrictive when renaming (simultaneous substitution),
so still absent from `BDD._rename`.

The assertion from b3ea0ec6fe27d8900327cd01b128e9e21aee1064 is required
in `BDD._swap`, in accord with the docstring of the CUDD function
`Cudd_bddSwapVariables` [1].

In addition, it is now asserted that each variable occurs in
at most one key-value pair. This assertion avoids `dvars` values as
for example `dict(x='y', z='y')`.

The assertion from 4ea0e9ae4373ee599163772dec0784c3497a50b3 that the
values of `dvars` are not in the support of the argument `u` is
unnecessary for both `_rename` and `_swap`. However, this assertion
is necessary when swapping is used to implement renaming, and in
that case is restrictive.

This bug was introduced in 4ea0e9ae4373ee599163772dec0784c3497a50b3,

dd/cudd.pyx
@@ -775,7 +775,41 @@ cdef class BDD(object):
         return wrap(self, r)
 
     cpdef Function _rename(self, Function u, dvars):
-        """Return node `u` after renaming variables in `dvars`."""
+        """Return node `u` after renaming variables in `dvars`.
+
+        The argument value `dvars = dict(x='y')` results in
+        variable `'x'` substituted by variable `'y'`.
+
+        The argument value `dvars = dict(x='y', y='x')` results in
+        simultaneous substitution of variable `'x'` by variable `'y'`
+        and of variable `'y'` by variable `'x'`.
+        """
+        rename = {k: self.var(v) for k, v in dvars.items()}
+        return self._compose(u, rename)
+
+    cpdef Function _swap(self, Function u, dvars):
+        """Return node `u` after swapping variable pairs in `dvars`.
+
+        Asserts that each variable occurs in at most one key-value pair
+        of the dictionary `dvars`.
+
+        The argument value `dvars = dict(x='y')` results in swapping
+        of variables `'x'` and `'y'`, which is equivalent to
+        simultaneous substitution of `'x'` by `'y'` and `'y'` by `'x'`.
+
+        So the argument value `dvars = dict(x='y')` has the same
+        result as calling `_rename` with `dvars = dict(x='y', y='x')`.
+        """
+        # assert that each variable occurs in at most one
+        # key-value pair of the dictionary `dvars`:
+        # 1) assert keys and values of `dvars` are disjoint sets
+        common = {var for var in dvars.values() if var in dvars}
+        assert not common, common
+        # 2) assert each value is unique
+        values = set(dvars.values())
+        assert len(dvars) == len(values), dvars
+        #
+        # call swapping
         n = len(dvars)
         cdef DdNode **x = <DdNode **> PyMem_Malloc(n * sizeof(DdNode *))
         cdef DdNode **y = <DdNode **> PyMem_Malloc(n * sizeof(DdNode *))

tests/cudd_test.py
@@ -528,10 +528,34 @@ def test_rename():
     not_w = bdd.apply('not', w)
     f_ = bdd.apply('or', z, not_w)
     assert f == f_, (f, f_)
-    # x -> x
+    # ensure substitution, not swapping
+    #
+    # f == LET x == y
+    #      IN x /\ y
+    u = bdd.apply('and', x, y)
+    # replace x with y, but leave y as is
+    d = dict(x='y')
+    f = bdd.let(d, u)
+    # THEOREM f <=> y
+    assert f == y, (f, y)
+    # f == LET x == y
+    #      IN x /\ ~ y
+    u = bdd.apply('and', x, ~ y)
+    d = dict(x='y')
+    f = bdd.let(d, u)
+    # THEOREM f <=> FALSE
+    assert f == bdd.false, f
+    # simultaneous substitution
+    #
+    # f == LET x == y1  (* x1, y1 correspond to *)
+    #          y == x1  (* x, y after substitution *)
+    #      IN x /\ ~ y
+    u = bdd.apply('and', x, ~ y)
+    # replace x with y, and simultaneously, y with x
     d = dict(x='y', y='x')
     f = bdd.let(d, u)
-    f_ = bdd.apply('or', ~ x, y)
+    f_ = bdd.apply('and', y, ~ x)
+    # THEOREM f <=> (~ x /\ y)
     assert f == f_, (f, f_)
     del x, y, not_y, z, w, not_w, u, f, f_
     # as method
@@ -541,6 +565,67 @@ def test_rename():
     y = bdd.let(d, x)
     assert y == y_, (y, y_)
     del x, y, y_
+
+
+def test_swap():
+    bdd = cudd.BDD()
+    bdd.declare('x', 'y')
+    x = bdd.var('x')
+    y = bdd.var('y')
+    # swap x and y
+    #
+    # This swap returns the same result as `bdd.let`
+    # with the same arguments, because `d` contains
+    # both `'x'` and `'y'` as keys.
+    #
+    # This result is obtained when the arguments are
+    # not checked for overlapping key-value pairs.
+    u = bdd.apply('and', x, ~ y)
+    d = dict(x='y', y='x')
+    with assert_raises(AssertionError):
+        f = bdd._swap(u, d)
+    # f_ = bdd.apply('and', ~ x, y)
+    # assert f == f_, (f, f_)
+    #
+    # swap x and y
+    # ensure swapping, not simultaneous substitution
+    #
+    # This swap returns a different result than
+    # `bdd.let` when given the same arguments,
+    # because `d` contains only `'x'` as key,
+    # so `let` does not result in simultaneous
+    # substitution.
+    u = bdd.apply('and', x, ~ y)
+    d = dict(x='y')  # swap x with y
+    f = bdd._swap(u, d)
+    f_ = bdd.apply('and', y, ~ x)
+    # compare to the corresponding test of `bdd.let`
+    assert f == f_, (f, f_)
+    #
+    # each variable should in at most one
+    # key-value pair of `d`
+    #
+    # 1) keys and values are disjoint sets
+    bdd.declare('z')
+    z = bdd.var('z')
+    u = bdd.apply('and', x, ~ y)
+    d = dict(x='y', y='z')
+    with assert_raises(AssertionError):
+        f = bdd._swap(u, d)
+    # The following result is obtained if the
+    # assertions are removed from `BDD._swap`.
+    # f_ = bdd.apply('and', y, ~ z)
+    # assert f == f_, bdd.to_expr(f)
+    #
+    # 2) each value appears once among values
+    u = bdd.apply('and', x, ~ y)
+    d = dict(x='y', z='y')
+    with assert_raises(AssertionError):
+        f = bdd._swap(u, d)
+    # The following result is obtained if the
+    # assertions are removed from `BDD._swap`.
+    # f_ = bdd.apply('and', y, ~ z)
+    # assert f == f_, bdd.to_expr(f)
 
 
 def test_ite():
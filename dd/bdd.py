tulip-control/dd

commit 7085484bb7b44c65cdc3675333fe6eadff0ae0be

ENH: allow simultaneous substitution in `dd.bdd.BDD.let`

dd/bdd.py
@@ -483,10 +483,7 @@ class BDD(_abc.BDD):
         if isinstance(value, bool):
             return self.cofactor(u, d)
         elif isinstance(value, int):
-            # `compose` will support multiple substitutions
-            # in the future
-            assert len(d) == 1, d
-            return self.compose(u, var, value)
+            return self.compose(u, d)
         try:
             value + 's'
         except TypeError:
@@ -497,17 +494,20 @@ class BDD(_abc.BDD):
         return self.rename(u, d)
 
     @_try_to_reorder
-    def compose(self, f, var, g):
-        """Return f(x_var=g).
+    def compose(self, f, var_sub):
+        """Return substitutions `var_sub` in `f`.
 
-        @param f, g: nodes
-        @param var: variable name
-        @param cache: stores intermediate results
+        @param f: node
+        @param var_sub: `dict` that maps variables to BDD nodes
         """
-        j = self.level_of_var(var)
         cache = dict()
-        u = self._compose(f, j, g, cache)
-        return u
+        if len(var_sub) == 1:
+            (var, g), = var_sub.items()
+            j = self.level_of_var(var)
+            r = self._compose(f, j, g, cache)
+        else:
+            r = _copy_bdd(f, var_sub, self, self, cache)
+        return r
 
     def _compose(self, f, j, g, cache):
         # terminal ?

commit cb37e477da07c8059a0d1830c605c2d9cf510ec5

BUG: correct simultaneous substitution in `dd.bdd.BDD.let`
introduced in commit
7085484bb7b44c65cdc3675333fe6eadff0ae0be.

dd/bdd.py
@@ -557,7 +557,10 @@ class BDD(_abc.BDD):
             j = self.level_of_var(var)
             r = self._compose(f, j, g, cache)
         else:
-            r = _copy_bdd(f, var_sub, self, self, cache)
+            dvars = {
+                self.level_of_var(var): g
+                for var, g in var_sub.items()}
+            r = self._vector_compose(f, dvars, cache)
         return r
 
     def _compose(self, f, j, g, cache):
@@ -587,6 +590,33 @@ class BDD(_abc.BDD):
             q = self._compose(f1, j, g1, cache)
             r = self.find_or_add(z, p, q)
         cache[(f, g)] = r
+        return r
+
+    def _vector_compose(self, f, level_sub, cache):
+        # terminal ?
+        if abs(f) == 1:
+            return f
+        # cached ?
+        r = cache.get(abs(f))
+        if r is not None:
+            assert r > 0, r
+            # complement ?
+            if f < 0:
+                r = -r
+            return r
+        # recurse
+        i, v, w = self._succ[abs(f)]
+        p = self._vector_compose(v, level_sub, cache)
+        q = self._vector_compose(w, level_sub, cache)
+        # map this level
+        var = self.var_at_level(i)
+        g = level_sub.get(i, self.var(var))
+        r = self.ite(g, q, p)
+        # memoize
+        cache[abs(f)] = r
+        # complement ?
+        if f < 0:
+            r = -r
         return r
 
     @_try_to_reorder
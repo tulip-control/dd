6b6df8e8bcadd283f15ece661dea972a6b398c72
Ioannis Filippidis <jfilippidis@gmail.com>
2021-05-07 17:23:31 +0200

    TST: assert that CUDD node reference count >= 0
    Although, for a given BDD/ZDD node, the methods
    `dd.cudd.BDD.decref` and `dd.cudd_zdd.ZDD.decref`
    were not intended to ever be called without first
    calling the corresponding `incref` methods,
    this test checks this kind of situation.
    
    A single call to `decref` without a corresponding
    earlier call to `incref` was intended to be
    possible, but in fact causes errors with the
    current implementation, due to the method
    `Function.__dealloc__` unconditionally
    dereferencing the node.
    
    So such a call to `decref` results, after the call to
    `Function.__dealloc__`, in an integer wraparound,
    and would have resulted in a *negative* reference count
    for the node, had the reference count not been represented
    by an unsigned integer within CUDD.
    
    An upcoming commit fixes this error.
    
    Also, the method `decref` is not intended to
    be called by the user at all. Instead, automated
    reference counting should be used.
    
    In case early release of memory is needed,
    then `del u` can be used, where `u` is an instance
    of the class `dd.cudd.Function` or of the class
    `dd.cudd_zdd.Function`.

--- tests/common_cudd.py
+++ tests/common_cudd.py
@@ -48,3 +48,117 @@ class Tests(object):
         assert x == 'x', x
         assert y == 'y', y
         assert z == 'z', z
+
+    def test_incref_decref_locally_inconsistent(self):
+        # "Locally inconsistent" here means that
+        # from the viewpoint of some `Function` instance,
+        # the calls to `incref` and `decref` would result
+        # in an incorrect reference count.
+        #
+        # In this example it so happens that overall
+        # the calls to `incref` and `decref` do not
+        # result in an incorrect reference count.
+        bdd = self.DD()
+        bdd.declare('x', 'y')
+        u = bdd.add_expr(r'x /\ y')
+        v = bdd.add_expr(r'x /\ y')
+        bdd.incref(u)
+        bdd.decref(v)
+        del u, v
+        assert len(bdd) == 0, len(bdd)
+
+    def test_decref_incref_locally_inconsistent(self):
+        # "Locally inconsistent" here means the
+        # same as described in the previous method.
+        #
+        # The difference with the previous method
+        # is that here `decref` is called before
+        # `incref`, not after.
+        #
+        # As in the previous method,
+        # overall the calls to `incref` and `decref`
+        # result in a correct reference count.
+        bdd = self.DD()
+        bdd.declare('x', 'y')
+        u = bdd.add_expr(r'x /\ y')
+        v = bdd.add_expr(r'x /\ y')
+        bdd.decref(v)
+        bdd.incref(u)
+        del u, v
+        assert len(bdd) == 0, len(bdd)
+
+    def test_double_incref_decref_locally_inconsistent(self):
+        # "Locally inconsistent" here means the
+        # same as described in a method above.
+        #
+        # The main difference with the previous method
+        # is that here `decref` is called twice.
+        #
+        # As in the previous method,
+        # overall the calls to `incref` and `decref`
+        # result in a correct reference count.
+        bdd = self.DD()
+        bdd.declare('x', 'y')
+        u = bdd.add_expr(r'x /\ y')
+        v = bdd.add_expr(r'x /\ y')
+        bdd.incref(u)
+        bdd.incref(u)
+        bdd.decref(v)
+        bdd.decref(v)
+        del u, v
+        assert len(bdd) == 0, len(bdd)
+
+    def test_decref_and_dealloc(self):
+        bdd = self.DD()
+        bdd.declare('x', 'y')
+        u = bdd.add_expr(r'x /\ ~ y')
+        assert u.ref == 1, u.ref
+        s = str(u)
+        bdd.decref(u, recursive=True)
+        del u  # calls method `Function.__dealloc__`
+        # the execution of `decref` and then
+        # `__dealloc__` should result in
+        # reference count 0,
+        # not a negative value
+        v = bdd.add_expr(s)
+        assert v.ref == 1, v.ref
+        del v
+        # the following check passes when
+        # `u.ref = 0 - 1`,
+        # because the reference count is an
+        # unsigned integer, so subtracting 1
+        # results in a saturated positive value,
+        # which is ignored by the function
+        # `Cudd_CheckZeroRef` (which checks
+        # `node->ref != 0 && node->ref != DD_MAXREF`)
+        assert len(bdd) == 0, len(bdd)
+
+    def test_decref(self):
+        bdd = self.DD()
+        # Turn off garbage collection to prevent the
+        # memory for the CUDD BDD/ZDD node below from
+        # being deallocated when the reference count
+        # of the node reaches 0.
+        # If that happened, then further access to
+        # the attribute `u.ref` would have been unsafe.
+        bdd.configure(
+            reordering=False,
+            garbage_collection=False)
+        bdd.declare('x', 'y')
+        u = bdd.add_expr('x /\ ~ y')
+        assert u.ref == 1, u.ref
+        bdd.decref(u, recursive=True)
+        # safe to access the CUDD BDD/ZDD node's
+        # reference count because garbage collection
+        # has been turned off above
+        assert u.ref == 0, u.ref
+        # Ensure that `decref` decrements
+        # only positive reference counts.
+        # No need for `recursive=True`,
+        # because this call should have
+        # no effect at all.
+        bdd.decref(u)
+        # safe to access the CUDD BDD/ZDD node's
+        # reference count because garbage collection
+        # has been turned off above
+        assert u.ref == 0, u.ref

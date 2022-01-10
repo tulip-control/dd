tulip-control/dd

commit 6f3ade0932097ec48938250a5c5437d9e10c66b9

ENH: add `cudd.BDD.to_expr`

dd/cudd.pyx
@@ -859,6 +859,33 @@ cdef class BDD(object):
         """Return node for `str` expression `e`."""
         return _parser.add_expr(e, self)
 
+    cpdef str to_expr(self, Function u):
+        """Return a Boolean expression for node `u`."""
+        assert u.manager == self.manager
+        return self._to_expr(u.node)
+
+    cdef str _to_expr(self, DdNode *u):
+        if u == Cudd_ReadLogicZero(self.manager):
+            return 'False'
+        if u == Cudd_ReadOne(self.manager):
+            return 'True'
+        r = Cudd_Regular(u)
+        i = r.index
+        v = Cudd_E(u)
+        w = Cudd_T(u)
+        var = self.var_at_level(i)
+        p = self._to_expr(v)
+        q = self._to_expr(w)
+        # pure var ?
+        if p == 'False' and q == 'True':
+            s = var
+        else:
+            s = 'ite({var}, {q}, {p})'.format(var=var, p=p, q=q)
+        # complemented ?
+        if Cudd_IsComplement(u):
+            s = '(! {s})'.format(s=s)
+        return s
+
     cpdef dump(self, Function u, fname):
         """Dump BDD as DDDMP file `fname`."""
         n = len(self._index_of_var)

commit f70a5a998ccb12ee8ef50f0fc1b3db6fc554f1cd

BUG: correctly map index to variable identifier
This error was introduced in commit
6f3ade0932097ec48938250a5c5437d9e10c66b9.

dd/cudd.pyx
@@ -1142,7 +1142,7 @@ cdef class BDD(object):
         i = r.index
         v = Cudd_E(u)
         w = Cudd_T(u)
-        var = self.var_at_level(i)
+        var = self._var_with_index[i]
         p = self._to_expr(v)
         q = self._to_expr(w)
         # pure var ?
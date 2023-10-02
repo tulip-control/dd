tulip-control/dd

commit 2fb70ab91d14ef1ac0722a32a0a04f91acaad73f

ENH: add module `dd.cudd_zdd`
and the scripts `tests/cudd_zdd_test.py` and
`examples/cudd_zdd.py`.

tests/cudd_zdd_test.py
@@ -1,0 +1,249 @@
+"""Tests for `dd.cudd_zdd`."""
+import os
+
+from dd import cudd
+from dd import cudd_zdd
+from dd import _copy
+
+from common import Tests as Tests
+from common_cudd import Tests as CuddTests
+
+
+Tests.DD = cudd_zdd.ZDD
+CuddTests.DD = cudd_zdd.ZDD
+
+
+def test_false():
+    zdd = cudd_zdd.ZDD()
+    u = zdd.false
+    assert len(u) == 0, len(u)
+
+
+def test_true():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z', 'w')
+    u = zdd.true
+    assert u.low is not None
+    assert u.high is not None
+    assert len(u) == 4, len(u)
+
+
+def test_true_node():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y')
+    u = zdd.true_node
+    assert u.low is None
+    assert u.high is None
+    assert len(u) == 0, len(u)
+
+
+def test_var():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    x = zdd.var('x')
+    x_ = zdd._var_cudd('x')
+    assert x == x_, len(x)
+    y = zdd.var('y')
+    y_ = zdd._var_cudd('y')
+    assert y == y_, len(y)
+    z = zdd.var('z')
+    z_ = zdd._var_cudd('z')
+    assert z == z_, len(z)
+
+
+def test_support_cudd():
+    # support implemented by CUDD
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y')
+    zdd._add_bdd_var(0)
+    zdd._add_bdd_var(1)
+    u = zdd.add_expr('~ x')
+    s = zdd._support_cudd(u)
+    assert s == {'y'}, s  # `{'x'}` is expected
+
+
+def test_cudd_cofactor():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y')
+    u = zdd.add_expr('x /\ ~ y')
+    r = zdd._cofactor_cudd(u, 'y', False)
+    r_ = zdd.add_expr('x /\ ~ y')
+    assert r == r_, len(r)
+    u = zdd.add_expr('x /\ y')
+    r = zdd._cofactor_cudd(u, 'x', True)
+    r_ = zdd.add_expr('~ x /\ y')  # no node at x
+    assert r == r_
+
+
+def test_find_or_add():
+    bdd = cudd_zdd.ZDD()
+    bdd.declare('x', 'y', 'z')
+    v = bdd.add_expr('~ x /\ y /\ ~ z')
+    w = bdd.add_expr('~ x /\ ~ y /\ z')
+    u = bdd.find_or_add('x', v, w)
+    assert u.low == v, len(u)
+    assert u.high == w, len(u)
+    assert u.var == 'x', u.var
+    assert u.level == 0, u.level
+
+
+def test_count():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y')
+    # FALSE
+    u = zdd.false
+    n = zdd.count(u, 2)
+    assert n == 0, n
+    # TRUE
+    u = zdd.true
+    n = zdd.count(u, 1)
+    assert n == 2, n
+    n = zdd.count(u, 2)
+    assert n == 4, n
+
+
+def test_bdd_to_zdd_copy():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    bdd = cudd.BDD()
+    bdd.declare('x', 'y', 'z')
+    u = bdd.add_expr('x')
+    v = bdd.copy(u, zdd)
+    x = zdd.var('x')
+    assert v == x, len(v)
+    print_size(v, 'v')
+    # copy `y`
+    u = bdd.var('y')
+    y = bdd.copy(u, zdd)
+    y_ = zdd.var('y')
+    assert y == y_, (y, y_)
+
+
+def test_len():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    # x
+    x = zdd.var('x')
+    assert len(x) == 3, len(x)
+    # y
+    y = zdd.var('y')
+    assert len(y) == 3, len(y)
+    # x /\ y /\ ~ z
+    u = x & y & ~ zdd.var('z')
+    assert len(u) == 2, len(u)
+    # ~ x
+    u = zdd.add_expr('~ x')
+    assert len(u) == 2, len(u)
+
+
+def test_disjunction():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('w', 'x', 'y')
+    # x \/ TRUE
+    v = zdd.add_expr('x')
+    w = zdd.true
+    u = zdd._disjoin_root(v, w)
+    assert u == w, len(u)
+    # x \/ FALSE
+    w = zdd.false
+    u = zdd._disjoin_root(v, w)
+    assert u == v, len(u)
+    # x \/ y
+    v = zdd.add_expr('x')
+    w = zdd.add_expr('y')
+    u = zdd._disjoin_root(v, w)
+    u_ = zdd.add_expr('x \/ y')
+    assert u == u_, len(u)
+    # (~ w /\ x) \/ y
+    v = zdd.add_expr('~ w /\ x')
+    w = zdd.add_expr('y')
+    u = zdd._disjoin_root(v, w)
+    u_ = zdd.add_expr('(~ w /\ x) \/ y')
+    assert u == u_, len(u)
+
+
+def test_conjunction():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    v = zdd.var('x')
+    w = zdd.var('y')
+    u = zdd._conjoin_root(v, w)
+    u_ = zdd.add_expr('x /\ y')
+    assert u == u_, len(u)
+    u = zdd._conjoin_root(v, ~ w)
+    u_ = zdd.add_expr('x /\ ~ y')
+    assert u == u_, len(u)
+
+
+def test_c_disjunction():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('w', 'x', 'y')
+    v = zdd.add_expr('~ w /\ x')
+    w = zdd.add_expr('y')
+    u = cudd_zdd._c_disjoin(v, w)
+    u_ = zdd.add_expr('(~ w /\ x) \/ y')
+    assert u == u_, len(u)
+
+
+def test_c_conjunction():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    x = zdd.var('x')
+    y = zdd.var('y')
+    u = cudd_zdd._c_conjoin(x, y)
+    u_ = zdd.add_expr('x /\ y')
+    assert u == u_, len(u)
+
+
+def test_c_exist():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    # \E x:  (x /\ ~ y) \/ ~ z
+    u = zdd.add_expr('(x /\ ~ y) \/ ~ z')
+    qvars = ['x']
+    r = cudd_zdd._c_exist(qvars, u)
+    r_ = zdd.exist(qvars, u)
+    assert r == r_, len(r)
+    # \E x:  x
+    u = zdd.add_expr('x')
+    qvars = ['x']
+    r = cudd_zdd._c_exist(qvars, u)
+    r_ = zdd.exist(qvars, u)
+    assert r == r_, len(r)
+
+
+def test_dump():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'w')
+    u = zdd.add_expr('~ w')
+    fname = 'not_w.pdf'
+    if os.path.isfile(fname):
+        os.remove(fname)
+    assert not os.path.isfile(fname)
+    zdd.dump(fname, [u])
+    assert os.path.isfile(fname)
+
+
+def test_dict_to_zdd():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    qvars = {'x', 'z'}
+    u = cudd_zdd._dict_to_zdd(qvars, zdd)
+    assert len(u) == 2, len(u)
+    assert u.var == 'x', u.var
+    assert u.low == u.high
+    v = u.low
+    assert v.var == 'z', v.var
+    assert v.low == v.high
+    assert v.low == zdd.true_node
+
+
+def print_size(u, msg):
+    n = len(u)
+    print('Dag size of {msg}: {n}'.format(
+        msg=msg, n=n))
+
+
+if __name__ == '__main__':
+    Tests().test_support()
+    # test_compose()

.travis.yml
@@ -36,7 +36,7 @@ install:
   - export LD_LIBRARY_PATH=`pwd`/src/.libs:$LD_LIBRARY_PATH
   - cd ..
   # dd
-  - python setup.py install --fetch --cudd --sylvan
+  - python setup.py install --fetch --cudd --cudd_zdd --sylvan
 
 script:
   - cd tests/

Makefile
@@ -56,6 +56,10 @@ reinstall:
 	-pip uninstall -y dd
 	python setup.py install --cudd
 
+reinstall_zdd:
+	-pip uninstall -y dd
+	python setup.py install --cudd --cudd_zdd
+
 uninstall:
 	-pip uninstall -y dd
 
@@ -88,6 +92,7 @@ clean:
 	-rm dd/*.so
 	-rm dd/buddy.c
 	-rm dd/cudd.c
+	-rm dd/cudd_zdd.c
 	-rm dd/sylvan.c
 	-rm *.pyc */*.pyc
 	-rm -rf __pycache__ */__pycache__

dd/_abc.py
@@ -4,6 +4,7 @@ This specification is implemented by the modules:
 
 - `dd.autoref`
 - `dd.cudd`
+- `dd.cudd_zdd`
 - `dd.sylvan` (partially)
 - `dd.buddy` (partially)
 """

dd/_copy.py
@@ -33,7 +33,7 @@ def copy_bdds_from(roots, target):
 def copy_bdd(u, target, cache=None):
     """Copy BDD with root `u` to manager `target`.
 
-    @param target: BDD
+    @param target: BDD or ZDD
     @param cache: `dict` for memoizing results
     """
     if cache is None:
@@ -80,6 +80,47 @@ def _copy_bdd(u, bdd, cache):
 def _flip(r, u):
     """Negate `r` if `u` is negated, else identity."""
     return ~ r if u.negated else r
+
+
+def copy_zdd(u, target, cache=None):
+    """Copy ZDD with root `u` to manager `target`.
+
+    @param target: BDD or ZDD
+    @param cache: `dict` for memoizing results
+    """
+    if cache is None:
+        cache = dict()
+    level = 0
+    r = _copy_zdd(level, u, target, cache)
+    return r
+
+
+def _copy_zdd(level, u, target, cache):
+    """Recurse to copy node `u` to `target`.
+
+    @type cache: `dict`
+    """
+    src = u.bdd
+    # terminal ?
+    if u == src.false:
+        return target.false
+    if level == len(src.vars):
+        return target.true
+    # memoized ?
+    k = int(u)
+    if k in cache:
+        return cache[k]
+    # recurse
+    v, w = src._top_cofactor(u, level)
+    low = _copy_zdd(level + 1, v, target, cache)
+    high = _copy_zdd(level + 1, w, target, cache)
+    # add node
+    var = src.var_at_level(level)
+    g = target.var(var)
+    r = target.ite(g, high, low)
+    # memoize
+    cache[k] = r
+    return r
 
 
 def dump_json(nodes, file_name):

download.py
@@ -25,7 +25,7 @@ except ImportError:
 from setuptools.extension import Extension
 
 
-EXTENSIONS = ['cudd', 'buddy', 'sylvan']
+EXTENSIONS = ['cudd', 'cudd_zdd', 'buddy', 'sylvan']
 # CUDD
 CUDD_VERSION = '3.0.0'
 CUDD_TARBALL = 'cudd-{v}.tar.gz'.format(v=CUDD_VERSION)
@@ -103,6 +103,13 @@ def extensions(args):
         cudd=Extension(
             'dd.cudd',
             sources=['dd/cudd' + pyx],
+            include_dirs=_join(cudd_include),
+            library_dirs=_join(cudd_link),
+            libraries=CUDD_LIB,
+            extra_compile_args=cudd_cflags),
+        cudd_zdd=Extension(
+            'dd.cudd_zdd',
+            sources=['dd/cudd_zdd' + pyx],
             include_dirs=_join(cudd_include),
             library_dirs=_join(cudd_link),
             libraries=CUDD_LIB,

examples/cudd_zdd.py
@@ -1,0 +1,16 @@
+"""How to use ZDDs with CUDD."""
+from dd import cudd_zdd
+
+
+def main():
+    zdd = cudd_zdd.ZDD()
+    zdd.declare('x', 'y', 'z')
+    u = zdd.add_expr('(x /\ y) \/ z')
+    let = dict(y=zdd.add_expr('~ x'))
+    v = zdd.let(let, u)
+    v_ = zdd.add_expr('z')
+    assert v == v_, (v, v_)
+
+
+if __name__ == '__main__':
+    main()

commit 0886a9e325ee3ec22539407b95d35c14fe8f3522

BUG: correct check for leaf nodes in `cudd_zdd`
These changes are corrections to ensure the intended behavior.
The API does not change.

Also, these are changes to internal `cdef` functions,
which are not callable from Python, so not in the API.

Several functions in `dd.cudd_zdd` were checking for
the leaf node as:

```cython
def function(level, u):
    ...
    index = Cudd_ReadInvPermZdd(mgr, level)
    if u == DD_ZERO(mgr) or index == -1:
        return u
    ...
```

`Cudd_ReadInvPermZdd()` returns `CUDD_CONST_INDEX` for
leaf nodes (their level). So passing:
`level=CUDD_CONST_INDEX, u=DD_ONE(mgr)`
leads to beyond the `if` statement.

Affected `cdef` functions of `dd.cudd_zdd`:
- `_forall()`
- `_exist()`
- `_disjoin()`
- `_conjoin()`
- `_support()`

Error introduced in commit
`2fb70ab91d14ef1ac0722a32a0a04f91acaad73f`,
which added `dd.cudd_zdd`.

The correction is to disjoin the predicate
`index == CUDD_CONST_INDEX`
where applicable.


## This error is impossible to occur

The function `Cudd_zddIthVar()`:

- returns `NULL` when attempting to create a CUDD variable
  index `i >= CUDD_MAXINDEX - 1`
  (`CUDD_MAXINDEX == CUDD_CONST_INDEX`).

- attempts to construct a `DdNode` for the ZDD that
  represents the variable, for index in
  `range(CUDD_MAXINDEX - 2)`.

So no CUDD node has level `CUDD_MAXINDEX - 1`.
Thus, no CUDD variable index is adjacent to the level of
leaf nodes, hence:

- `Cudd_ReadInvPermZdd()`
- `Cudd_ReadInvPerm()`

always return a `-1` for at least one integer
between the level of the bottommost variable index,
and the level of leaf nodes.

Besides, any call to `Cudd_zddIthVar()` for a variable at
level `CUDD_MAXINDEX - 1` would attempt to produce a ZDD
with `CUDD_CONST_INDEX + 1` nodes, which would require GBs
of memory.

```c
#include <stdio.h>
#include "cudd.h"
#include "cuddInt.h"

int main(){
    printf("%ld\n", sizeof(DdNode));
    return 0;
}
```

(Shifting a variable declared at a smaller level,

dd/cudd_zdd.pyx
@@ -2768,7 +2768,9 @@ cdef DdNode *_forall(
     if cube is NULL:
         raise AssertionError('`cube is NULL`')
     index = Cudd_ReadInvPermZdd(mgr, level)
-    if u == DD_ZERO(mgr) or index == -1:
+    if (u == DD_ZERO(mgr) or
+            index == -1 or
+            index == CUDD_CONST_INDEX):
         return u
     r = cuddCacheLookup2Zdd(
         mgr, _forall_cache_id, u, cube)
@@ -2887,7 +2889,9 @@ cdef DdNode *_exist(
     if cube is NULL:
         raise AssertionError('`cube is NULL`')
     index = Cudd_ReadInvPermZdd(mgr, level)
-    if u == DD_ZERO(mgr) or index == -1:
+    if (u == DD_ZERO(mgr) or
+            index == -1 or
+            index == CUDD_CONST_INDEX):
         return u
     r = cuddCacheLookup2Zdd(
         mgr, _exist_cache_id, u, cube)
@@ -3074,7 +3078,7 @@ cdef DdNode *_disjoin(
         return v
     if v == DD_ZERO(mgr):
         return u
-    if index == -1:
+    if index == -1 or index == CUDD_CONST_INDEX:
         if u != DD_ONE(mgr):
             raise AssertionError(
                 'nonconstant node `u` given, '
@@ -3203,7 +3207,7 @@ cdef DdNode *_conjoin(
         return u
     if v == DD_ZERO(mgr):
         return v
-    if index == -1:
+    if index == -1 or index == CUDD_CONST_INDEX:
         if u != DD_ONE(mgr):
             raise AssertionError(
                 'nonconstant node `u` given, '
@@ -3519,7 +3523,9 @@ cdef bint _support(
         raise AssertionError('`support is NULL`')
     index = Cudd_ReadInvPermZdd(mgr, level)
     # terminal ?
-    if u == DD_ZERO(mgr) or index == -1:
+    if (u == DD_ZERO(mgr) or
+            index == -1 or
+            index == CUDD_CONST_INDEX):
         return True
     # visited ?
     if Cudd_IsComplement(u.next):

tests/cudd_zdd_test.py
@@ -509,6 +509,26 @@ def test_c_disjoin_conjoin():
         cudd_zdd._call_conjoin(level, true, u)
 
 
+def test_c_disjoin_conjoin_leaf_check():
+    zdd = cudd_zdd.ZDD()
+    leaf_level = zdd.false.level
+    u = zdd.true_node
+    r = cudd_zdd._call_disjoin(
+        leaf_level, u, u)
+    assert r == u, (r.level, u.level)
+    zdd.declare('x')
+    v = zdd.var('x')
+    with pytest.raises(AssertionError):
+        cudd_zdd._call_disjoin(
+            leaf_level, v, v)
+    r = cudd_zdd._call_conjoin(
+        leaf_level, u, u)
+    assert r == u, (r.level, u.level)
+    with pytest.raises(AssertionError):
+        cudd_zdd._call_conjoin(
+            leaf_level, v, v)
+
+
 def test_c_exist():
     zdd = cudd_zdd.ZDD()
     zdd.declare('x', 'y', 'z')
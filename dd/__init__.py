58dd51b8335c9505235d5a86a443ca856b666684
SaiCharanMarrivada <92221348+SaiCharanMarrivada@users.noreply.github.com>
2023-10-16 11:05:15 +0530

    Update __init__.py

--- dd/__init__.py
+++ dd/__init__.py
@@ -1,4 +1,30 @@
 """Package of algorithms based on decision diagrams."""
+from enum import Enum,auto
+
+class Cudd_ReorderingType(Enum):
+    CUDD_REORDER_SAME = auto()
+    CUDD_REORDER_NONE = auto()
+    CUDD_REORDER_RANDOM = auto()
+    CUDD_REORDER_RANDOM_PIVOT = auto()
+    CUDD_REORDER_SIFT = auto()
+    CUDD_REORDER_SIFT_CONVERGE = auto()
+    CUDD_REORDER_SYMM_SIFT = auto()
+    CUDD_REORDER_SYMM_SIFT_CONV = auto()
+    CUDD_REORDER_WINDOW2 = auto()
+    CUDD_REORDER_WINDOW3 = auto()
+    CUDD_REORDER_WINDOW4 = auto()
+    CUDD_REORDER_WINDOW2_CONV = auto()
+    CUDD_REORDER_WINDOW3_CONV = auto()
+    CUDD_REORDER_WINDOW4_CONV = auto()
+    CUDD_REORDER_GROUP_SIFT = auto()
+    CUDD_REORDER_GROUP_SIFT_CONV = auto()
+    CUDD_REORDER_ANNEALING = auto()
+    CUDD_REORDER_GENETIC = auto()
+    CUDD_REORDER_LINEAR = auto()
+    CUDD_REORDER_LINEAR_CONVERGE = auto()
+    CUDD_REORDER_LAZY_SIFT = auto()
+    CUDD_REORDER_EXACT = auto()
+
 try:
     from ._version import version as __version__
 except ImportError:
@@ -8,3 +34,5 @@ try:
 except ImportError:
     from dd import autoref as _bdd
 BDD = _bdd.BDD
+
+__all__ = [Cudd_ReorderingType, BDD]

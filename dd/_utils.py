tulip-control/dd

commit a858fa756cb0b27d74cf113ff73efadc05c8b856

ENH: implement all comparisons in `cudd.Function.__richcmp__`

dd/cudd.pyx
@@ -1441,12 +1441,21 @@ cdef class Function(object):
             # guard against mixing managers
             assert self.manager == other.manager
             eq = (self.node == other.node)
-        if op == 2:
+        if op == 2:  # ==
             return eq
-        elif op == 3:
+        elif op == 3:  # !=
             return not eq
+        elif op == 0:  # <
+            return (other | ~ self) == self.bdd.true and not eq
+        elif op == 1:  # <=
+            return (other | ~ self) == self.bdd.true
+        elif op == 4:  # >
+            return (self | ~ other) == self.bdd.true and not eq
+        elif op == 5:  # >=
+            return (self | ~ other) == self.bdd.true
         else:
-            raise TypeError('Only `__eq__` and `__ne__` defined.')
+            raise ValueError(
+                'unexpected `op` value: {op}'.format(op=op))
 
     def __invert__(self):
         cdef DdNode *r

commit 4bf2be00814a21924f9bc047905d5be6fb979ce7

BUG: unsupported cases of `other is None` in `__richcmp__()`
Before this commit, when `other=None` was passed to:
- `dd.cudd.Function.__richcmp__()` and
- `dd.cudd_zdd.Function.__richcmp__()`
they did not raise the correct exceptions.

This commit corrects `__richcmp__()` so that now it
detects these cases of erroneous input, and raises a
`ValueError` with a more informative message than
the messages of the exceptions that were raised
before this commit.


## Description of error

`dd.cudd.Function.__richcmp__()` raised either a
suitable type of exception from an unexpected location,
and not with the intended message:

```python
u < None
    # ValueError: `self.manager != other.manager`
u <= None
    # ValueError: `self.manager != other.manager`
```

or another type of exception:

```python
u > None
    # TypeError: bad operand type for unary ~: 'NoneType'
u >= None
    # TypeError: bad operand type for unary ~: 'NoneType'
```

This bug was introduced in commit
`a858fa756cb0b27d74cf113ff73efadc05c8b856`.

dd/_utils.py
@@ -3,6 +3,20 @@
 # All rights reserved. Licensed under 3-clause BSD.
 #
 import textwrap as _tw
+import typing as _ty
+
+
+# The mapping from values of argument `op` of
+# `__richcmp__()` of Cython objects,
+# to the corresponding operator symbols.
+# Constants are defined in `cpython.object`.
+_CY_SYMBOLS: _ty.Final = {
+    2: '==',
+    3: '!=',
+    0: '<',
+    1: '<=',
+    4: '>',
+    5: '>='}
 
 
 def print_var_levels(bdd):

dd/cudd.pyx
@@ -2257,12 +2257,25 @@ cdef class Function:
 
     def __richcmp__(Function self, Function other, op):
         if other is None:
-            eq = False
-        else:
-            # guard against mixing managers
-            if self.manager != other.manager:
-                raise ValueError('`self.manager != other.manager`')
-            eq = (self.node == other.node)
+            if op == cpo.Py_EQ:
+                return False
+            elif op == cpo.Py_NE:
+                return True
+            elif op not in {
+                    cpo.Py_LT, cpo.Py_LE,
+                    cpo.Py_GT, cpo.Py_GE}:
+                raise ValueError(
+                    f'unexpected `op` value: {op} ')
+            symbol = _utils._CY_SYMBOLS[op]
+            raise ValueError(
+                f'unexpected `op` value: {op} '
+                f'(the value {op} represents '
+                f'"{symbol}")')
+        # guard against mixing managers
+        if self.manager != other.manager:
+            raise ValueError(
+                '`self.manager != other.manager`')
+        eq = (self.node == other.node)
         if op == cpo.Py_EQ:
             return eq
         elif op == cpo.Py_NE:

dd/cudd_zdd.pyx
@@ -2308,12 +2308,25 @@ cdef class Function:
 
     def __richcmp__(Function self, Function other, op):
         if other is None:
-            eq = False
-        else:
-            # guard against mixing managers
-            if self.manager != other.manager:
-                raise ValueError('`self.manager != other.manager`')
-            eq = (self.node == other.node)
+            if op == cpo.Py_EQ:
+                return False
+            elif op == cpo.Py_NE:
+                return True
+            elif op not in {
+                    cpo.Py_LT, cpo.Py_LE,
+                    cpo.Py_GT, cpo.Py_GE}:
+                raise ValueError(
+                    f'unexpected `op` value: {op}')
+            symbol = _utils.CY_SYMBOLS[op]
+            raise ValueError(
+                f'unexpected `op` value: {op} '
+                f'(the value {op} represents '
+                f'"{symbol}")')
+        # guard against mixing managers
+        if self.manager != other.manager:
+            raise ValueError(
+                '`self.manager != other.manager`')
+        eq = (self.node == other.node)
         if op == cpo.Py_EQ:
             return eq
         elif op == cpo.Py_NE:
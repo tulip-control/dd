tulip-control/dd

commit dc4ee404dd072460934c3aea86c9ed409946981b

API: add JSON to `dd.cudd.BDD.dump` and `load`

dd/cudd.pyx
@@ -1152,6 +1152,10 @@ cdef class BDD(object):
                 filetype == 'dddmp'):
             u, = roots  # single root supported for now
             return self._dump_dddmp(u, filename)
+        elif (
+                filename.lower().endswith('.json') or
+                filetype == 'json'):
+            return _copy.dump_json(roots, filename)
         else:
             bdd = autoref.BDD()
             _copy.copy_vars(self, bdd)  # preserve levels
@@ -1193,6 +1197,8 @@ cdef class BDD(object):
         if filename.lower().endswith('.dddmp'):
             r = self._load_dddmp(filename)
             return [r]
+        elif filename.lower().endswith('.json'):
+            return _copy.load_json(filename, self)
         else:
             raise ValueError(
                 'Unknown file type "{s}"'.format(

commit 48baf01dd5230f5fa25270896c9ed66dee927967

BUG: avoid unspecified return value
`dd._copy.dump_json()` returns `None` due to Python
default behavior in absence of a `return` statement.

Returning this value lets the API of `cudd.BDD.dump()`
implicitly change if `dd._copy.dump_json()` changes,
to then return a value unspecified for `dump()`.
Currently, the API is to `return` the default `None`.

Same change for the DDDMP case.

This bug was introduced in commit:
`dc4ee404dd072460934c3aea86c9ed409946981b`

dd/cudd.pyx
@@ -1852,9 +1852,11 @@ cdef class BDD:
         if filetype == 'dddmp':
             # single root supported for now
             u, = roots
-            return self._dump_dddmp(u, filename)
+            self._dump_dddmp(u, filename)
+            return
         elif filetype == 'json':
-            return _copy.dump_json(roots, filename)
+            _copy.dump_json(roots, filename)
+            return
         elif filetype not in (
                 'pickle', 'pdf', 'png', 'svg'):
             raise ValueError(filetype)
tulip-control/dd

commit cb37e477da07c8059a0d1830c605c2d9cf510ec5

BUG: correct simultaneous substitution in `dd.bdd.BDD.let`
introduced in commit
7085484bb7b44c65cdc3675333fe6eadff0ae0be.

dd/bdd.py
@@ -557,7 +557,10 @@ class BDD(_abc.BDD):
             j = self.level_of_var(var)
             r = self._compose(f, j, g, cache)
         else:
-            r = _copy_bdd(f, var_sub, self, self, cache)
+            dvars = {
+                self.level_of_var(var): g
+                for var, g in var_sub.items()}
+            r = self._vector_compose(f, dvars, cache)
         return r
 
     def _compose(self, f, j, g, cache):
@@ -587,6 +590,33 @@ class BDD(_abc.BDD):
             q = self._compose(f1, j, g1, cache)
             r = self.find_or_add(z, p, q)
         cache[(f, g)] = r
+        return r
+
+    def _vector_compose(self, f, level_sub, cache):
+        # terminal ?
+        if abs(f) == 1:
+            return f
+        # cached ?
+        r = cache.get(abs(f))
+        if r is not None:
+            assert r > 0, r
+            # complement ?
+            if f < 0:
+                r = -r
+            return r
+        # recurse
+        i, v, w = self._succ[abs(f)]
+        p = self._vector_compose(v, level_sub, cache)
+        q = self._vector_compose(w, level_sub, cache)
+        # map this level
+        var = self.var_at_level(i)
+        g = level_sub.get(i, self.var(var))
+        r = self.ite(g, q, p)
+        # memoize
+        cache[abs(f)] = r
+        # complement ?
+        if f < 0:
+            r = -r
         return r
 
     @_try_to_reorder

commit 8d8148c4db2fc24775051d09df1df93ae8678d24

BUG: correct assertion in `dd.bdd.BDD._vector_compose()`
Mapping a BDD node `abs(f)` with the ternary
conditional `dd.bdd.BDD.ite()` (IF...THEN...ELSE)
can result in `r` being an edge that
is not itself a node, but negated.

Negated edges are represented by negative integers.
So `r` can take both negative and positive integer values.
`r` cannot be zero, because no node is represented by
the number 0.

This error was introduced in commit:
`cb37e477da07c8059a0d1830c605c2d9cf510ec5`,
due to reading `dd.bdd._copy_bdd()` while writing
`dd.bdd.BDD._vector_compose()`.

These two functions are similar,
yet differ in how they map polarity:

- `dd.bdd.BDD._vector_compose()` (in general) changes
  the Boolean function that the BDD represents.
  So the polarity of nodes can change after the call
  to `BDD.ite()`.

- `dd.bdd._copy_bdd()` preserves the Boolean function
  that *each* node represents. Thus, the polarity
  remains unchanged after applying `BDD.ite()`,
  by BDD canonicity.

Therefore, asserting that `r > 0`:
- is correct in `_copy_bdd()`,
- but not in `BDD._vector_compose()`.

dd/bdd.py
@@ -887,7 +887,7 @@ class BDD(dd._abc.BDD):
         # cached ?
         r = cache.get(abs(f))
         if r is not None:
-            if r <= 0:
+            if r == 0:
                 raise AssertionError(r)
             # complement ?
             if f < 0: